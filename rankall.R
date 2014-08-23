e897db46d86b69a0afcf8ba1ca211d3fc20ce45a
rennyatwork <rennyatwork@gmail.com>
2014-08-23 11:59:48 -0400

    rankAll
    First check in
    not working yet (NA)

---
+++ rankall.R
@@ -0,0 +1,172 @@
+
+#setwd("C:/Users/renato/Documents/MOOC/2014/course02- R programming/Assignments/Assing-03-HospQuality/")
+# x <- getFile()
+#  y <- getRankedDataSet(x, "TX", "heart failure")
+## map of valid outcomes
+getValidOutcomes<-function()
+{
+  if(!exists("validOutcomes"))
+  {
+    validOutcomes <<- list("heart attack"= "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack" ,
+                           "heart failure" = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure" ,
+                           "pneumonia" = "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")
+  }
+  
+  return(validOutcomes)
+}
+
+## List of valid states
+getValidStates <-function()
+{  
+  if(!exists("validStates"))
+  {
+    validStates <<- c("AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", 
+                      "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
+                      "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+                      "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+                      "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY");
+  }
+  return (sort(validStates))
+}
+
+## Contains the words "best" and "worst"
+getValidNum <-function()
+{
+  if(!exists("validNum"))
+  {
+    validNum <<- c("best", "worst")
+  }
+  return (validNum);
+}
+
+
+getFile <-function(pFileName="outcome-of-care-measures.csv")
+{
+  pathToFile=paste(getwd(), "/", "rprog_data_ProgAssignment3-data/", pFileName, sep="")
+  if (!exists("dsFromFile"))
+  {
+    dsFromFile <<- read.csv(pathToFile, colClasses="character")
+  }
+  return (dsFromFile)
+}
+
+#checks whether a state is valid or not
+isValidState <-function(pState)
+{
+  return (any(getValidStates()==pState))
+}
+
+
+#checks whether an outcome is valid or not
+isValidOutcome <-function(pOutcome)
+{
+  #print("isValidOutcome")
+  #print("pOutcome: ") 
+  #print(pOutcome)
+  #print(as.character(getValidOutcomes()[pOutcome]))
+  return (as.character(getValidOutcomes()[pOutcome]) != "NULL")
+}
+
+
+
+#checkes whether a num argument is valid or not 
+isValidNum <-function(pNum)
+{
+  #if it's a string different from worse and best, return false
+  if(!is.numeric(pNum) && !any(getValidNum()==pNum))
+  {
+    return (FALSE)
+  }
+  
+  return((any(getValidNum()==pNum) || (pNum==floor(pNum)) ))
+}
+
+## returns a dataset with min values per state
+### testing example: minDs <- getRankedDataSet(x,  "heart attack")
+getRankedDataSet <- function(pDataSet,  pOutcome)
+{  
+  
+  ## gets the name of the outcome column
+  colOutcome <- as.character(getValidOutcomes()[pOutcome])
+  
+  
+  ## transforming the outcome column in numeric
+  pDataSet[,colOutcome] <- as.numeric(pDataSet[,colOutcome])
+  
+  ## subset by state eliminating NA
+  tempDs <- subset(pDataSet, !is.na(pDataSet[,colOutcome]), c("Hospital.Name", "City", "State", colOutcome))
+  
+   
+  
+  ##then, the minimum from the state ordered by outcome and hospital name
+  #minDs <- subset(tempDs, tempDs[colOutcome]== min(tempDs[colOutcome]))[order(colOutcome, "Hospital.Name"),]
+  minDs <- subset(tempDs,1==1)[order( tempDs[,"State"], tempDs[,colOutcome], tempDs[,"Hospital.Name"]),]
+  return (minDs)
+}
+
+
+getDfHospitalName <- function(pDataset, pIndex)
+{
+  dfHospital <- data.frame()
+  
+  for(st in getValidStates())
+  {
+    #print("st in getValidaStates()")
+    #print(st)
+    
+    if(pIndex == "best")
+    {
+      #print("best")
+      #return(as.character(head(pDataset$"Hospital.Name",1)[]))
+      #print(head(subset(pDataset, pDataset$State == st, c("Hospital.Name", "State")),1))
+      dfH<- head(subset(pDataset, pDataset$State == st, c("Hospital.Name", "State")),1)
+      dfHospital <- rbind(dfHospital,dfH)
+      
+    }
+    else if (pIndex=="worst")
+    {      
+      #print("worst")
+      dfHospital <- rbind(dfHospital,tail(subset(pDataset, pDataset$State == st, c("Hospital.Name", "State")),1))
+    } 
+    else
+    {
+      #print("pIndex")
+      dfHospital <- rbind(dfHospital,subset(pDataset, pDataset$State == st, c("Hospital.Name", "State"))[pIndex,])
+    }
+  }
+  
+  
+  
+  #print("pIndex")
+  #print(dfHospital)
+  return (dfHospital)
+}  
+
+
+rankall <- function(pOutcome, pNum = "best") {
+  ## Read outcome data
+  ## Check that state and outcome are valid
+  ## Return hospital name in that state with the given rank
+  ## 30-day death rate
+  
+  
+  
+  if(!isValidOutcome(pOutcome))
+  {
+    stop("invalid outcome")
+  }
+  if(!isValidNum(pNum))
+  {
+    stop("invalid num")
+  }
+  
+  dsRankedDataSet <- getRankedDataSet(getFile(), pOutcome)
+  
+  #print("head")
+  #print(head(dsRankedDataSet))
+  #print("tail")
+  #print(tail(dsRankedDataSet))
+  
+  return(getDfHospitalName(dsRankedDataSet, pNum))
+  
+}
